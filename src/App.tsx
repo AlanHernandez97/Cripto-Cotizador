7fd227305d805789cd6d404e0dc4ebd719809fd4
Alan Lopez <yondaime_97@live.com>
2024-06-20 00:06:57 -0600

    Proyecto finalizado

---
+++ src/App.tsx
@@ -0,0 +1,30 @@
+import { useEffect } from "react";
+import CriptoSearchForm from "./components/CriptoSearchForm"
+import { useCryptoStore } from "./store"
+import { CryptoPriceDisplay } from "./components/CryptoPriceDisplay";
+
+
+function App() {
+
+  const fetchCryptos = useCryptoStore(({ fetchCryptos }) => fetchCryptos);
+
+  useEffect(() => {
+    fetchCryptos()
+
+  }, [])
+
+
+  return (
+    <>
+      <div className="container">
+        <h1 className='app-title'>Cotizador de <span>Cirptomonedas</span></h1>
+        <div className="content">
+          <CriptoSearchForm />
+          <CryptoPriceDisplay />
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default App
